54b190008ede8717ad665ee650b4f13892c8f676
cianclarke <cian@cianclarke.com>
2013-12-16 14:01:02 -0500

    Twilio cloud service

--- cloud/main.js
+++ cloud/main.js
@@ -1,3 +1,19 @@
-exports = {
-  
-}
+/*
+ Sends an SMS with the twilio API
+ @param to : recipient
+ @param body : message body
+ */
+exports.twillio = function(params, cb){
+  // Your accountSid and authToken from twilio.com/user/account
+  var accountSid = process.env.TWILIO_SID,
+  authToken = process.env.TWILIO_AUTH,
+  client = require('twilio')(accountSid, authToken);
+
+  client.sms.messages.create({
+    body: params.body,
+    to: params.to,
+    from: process.env.TWILIO_NUMBER
+  }, function(err, message) {
+    return cb(err, message);
+  });
+};
