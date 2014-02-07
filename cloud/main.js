0c7a043ace763bb0d4849c03a231eb81bdf58bfd
cianclarke <cian@cianclarke.com>
2014-02-07 15:22:57 -0500

    Update template & rename twillio fn to sms

--- cloud/main.js
+++ cloud/main.js
@@ -3,7 +3,7 @@
  @param to : recipient
  @param body : message body
  */
-exports.twillio = function(params, cb){
+exports.sms = function(params, cb){
   // Your accountSid and authToken from twilio.com/user/account
   var accountSid = process.env.TWILIO_SID,
   authToken = process.env.TWILIO_AUTH,
